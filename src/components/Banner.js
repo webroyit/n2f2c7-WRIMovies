c7c1d7afa34a9a73ae550a0fda9403eb8acd1131
royweb <xytank@gmail.com>
2021-03-01 13:47:23 -0500

    Add a function to truncate the movie description

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -19,6 +19,10 @@ function Banner() {
         fetchData();
     }, []);
 
+    function truncate(str, n) {
+        return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+    }
+
     return (
         <header className="banner"
             style={{
@@ -36,7 +40,7 @@ function Banner() {
                     <button className="banner__button">My List</button>
                 </div>
                 <h2 className="banner__description">
-                    {movie?.overview}
+                    {truncate(movie?.overview, 150)}
                 </h2>
             </div>
         </header>
