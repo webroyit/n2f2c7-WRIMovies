593de0a14536247261f113c6fd7fd66f4826771d
royweb <xytank@gmail.com>
2021-03-01 12:15:46 -0500

    Add banner component

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 - When you use `export default file`, you can rename it when you import this file
 - You can only have one default export in a file
 - `overflow-x: scroll;` allow scrolling on horizontal
+- '?' is called optional chaining which is used to prevent the code from breaking if the value is undefined. (ex - movie?.title)
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -18,11 +18,26 @@ function Banner() {
         fetchData();
     }, []);
 
-    console.log(movie);
-
     return (
-        <header>
-            
+        <header className="banner"
+            style={{
+                backgroundSize: "cover",
+                backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie?.backdrop_path})`,
+                backgroundPosition: "center center"
+            }}
+        >
+            <div className="banner__contents">
+                <h1>
+                    {movie?.title || movie?.name || movie?.original_name}
+                </h1>
+                <div className="banner__buttons">
+                    <button className="banner__button">Play</button>
+                    <button className="banner__button">My List</button>
+                </div>
+                <h2 className="banner__description">
+                    {movie?.overview}
+                </h2>
+            </div>
         </header>
     )
 }
